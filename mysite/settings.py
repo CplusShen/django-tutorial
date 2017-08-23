81b812fe98f9f4a7f608c0082506c8f9ff0c6a07
Cplus Shen <cplus.shen@advantech.com.tw>
2017-08-23 17:18:04 +0800

    added a shortcut: get_object_or_404()

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nvzu@tce*pysdgbsxnaaag1e3h9%hlkkddg1q7bw=5ijb*luo*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
